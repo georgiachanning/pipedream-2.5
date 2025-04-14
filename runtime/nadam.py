c3ec8c83d9d954302ccca41eb006c0b018eae0f3
Georgia Channing <georgia.channing@live.com>
2025-04-14 06:02:39 +0100

    adding NAdam

---
+++ runtime/nadam.py
@@ -0,0 +1,23 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+from torch.optim.optimizer import required
+
+from optimizer import OptimizerWithWeightStashing
+
+class NAdamWithWeightStashing(OptimizerWithWeightStashing):
+    """
+    Adam optimizer with weight stashing.
+    """
+    def __init__(self, modules, master_parameters, model_parameters,
+                 loss_scale, num_versions, lr=required, betas=(0.9,0.999), 
+                 weight_decay=0, verbose_freq=0, macrobatch=False,
+                 eps=1e-08, momentum_decay=0.004):
+        super(NAdamWithWeightStashing, self).__init__(
+            optim_name='NAdam',
+            modules=modules, master_parameters=master_parameters,
+            model_parameters=model_parameters, loss_scale=loss_scale,
+            num_versions=num_versions, lr=lr, betas=betas,
+            weight_decay=weight_decay, verbose_freq=verbose_freq,
+            macrobatch=macrobatch, eps=eps, momentum_decay=momentum_decay
+        )
--- runtime/nano-llm/models/nano/gpus=4/config.py
+++ runtime/nano-llm/models/nano/gpus=4/config.py
@@ -1,6 +1,8 @@
 from nanoGPT.model import GPTConfig
 
 def get_gpt_config():
+    ## bigger GPT
+
     return GPTConfig(
         vocab_size=50304,
         block_size=1024,
@@ -10,3 +12,17 @@ def get_gpt_config():
         dropout=0.0,
         bias=False
     )
+
+    ## baby GPT
+    ## for shakespeare char
+
+    '''return GPTConfig(
+        vocab_size=65,
+        block_size=256,
+        n_layer=6,
+        n_head=6,
+        n_embd=384,
+        dropout=0.2,
+        bias=False
+    )'''
+    
